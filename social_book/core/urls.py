143b209d2391a6355468f2d181c27f6624024b0e
Sourav Sambit Behera <souravsambit12@gmail.com>
2022-05-31 11:24:11 +0530

    commit

--- social_book/core/urls.py
+++ social_book/core/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('logout', views.logout, name='logout'),
+
+
 ]
